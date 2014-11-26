b0e99c5bcab95d08fd627e0b68fdb8c11c586a31
Mada <madalina.ungur@gmail.com>
2014-11-26 13:52:33 +0100

    This is a bubble sort small exercise, unrelated to the main project

---
+++ Test_sorting/src/Sorter.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+
+public class Sorter {
+
+	public static void main(String[] args) {
+		int myArray[] ={70, 60, 26, 9};
+		System.out.println("My initial array is: " + Arrays.toString(myArray));
+
+		for (int i=0; i< myArray.length-1; i++)
+		{
+			for (int j=i+1; j< myArray.length; j++)
+			{
+				if (myArray[i] > myArray[j])
+				{
+					int tempValue = myArray[j];
+					myArray[j] = myArray[i];
+					myArray[i] = tempValue;
+				}
+					
+			}
+		}
+		System.out.println("My sorted array is:" + Arrays.toString(myArray));
+	}
+
+}
